3fe34b8c50db72fd7f252008d8090c971bd410fc
jartsanull <langinenj1@gmail.com>
2025-04-10 00:02:09 +0300

    First func

--- src/features/excel/extract_from_excel.py
+++ src/features/excel/extract_from_excel.py
@@ -1,5 +1,16 @@
 import pandas as pd
 
+excel_input = input("")
+excel_reader = input("")
+
+excel_choices = ["read"]
+
 def read_excel():
     # Reading an Excel file
-    df = pd.read_excel('file.xlsx', sheet_name='Sheet1')
+    df = pd.read_excel(excel_reader, sheet_name='Sheet1')
+
+match excel_choices:
+    case "read":
+        read_excel()
+    case _:
+        print("Invalid choice")
